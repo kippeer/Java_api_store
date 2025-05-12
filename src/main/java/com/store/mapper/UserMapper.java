af55abce2ddc8c99d8a3b72d14399889e63ae96e
Fabio <fabiot92@gmail.com>
2025-05-12 17:14:48 -0300

    Create UserMapper.java

---
+++ src/main/java/com/store/mapper/UserMapper.java
@@ -0,0 +1,31 @@
+package com.store.mapper;
+
+import com.store.dto.UserDTO;
+import com.store.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    @Mapping(target = "password", ignore = true)
+    UserDTO toDTO(User user);
+
+    List<UserDTO> toDTOList(List<User> users);
+
+    @Mapping(target = "authorities", ignore = true)
+    @Mapping(target = "accountNonExpired", ignore = true)
+    @Mapping(target = "accountNonLocked", ignore = true)
+    @Mapping(target = "credentialsNonExpired", ignore = true)
+    @Mapping(target = "orders", ignore = true)
+    User toEntity(UserDTO userDTO);
+
+    @Mapping(target = "password", ignore = true)
+    void updateEntityFromDTO(UserDTO userDTO, @MappingTarget User user);
+}
